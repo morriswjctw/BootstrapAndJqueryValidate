967a2b3bc92875cacd396ee8bf6b8da924dc5128
morriswjctw <morris.wjc.tw@gmail.com>
2017-03-10 18:18:56 +0800

    Bootstrap元件練習

--- BootstrapJqueryValidate/App_Start/BundleConfig.cs
+++ BootstrapJqueryValidate/App_Start/BundleConfig.cs
@@ -12,7 +12,8 @@ namespace BootstrapJqueryValidate
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/messages_zh_TW*"));
 
             // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
             // 準備好實際執行時，請使用 http://modernizr.com 上的建置工具，只選擇您需要的測試。
