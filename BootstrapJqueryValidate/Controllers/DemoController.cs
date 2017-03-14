d90d0946e58991f2d21065bdbddefcf3b5c59555
morriswjctw <morris.wjc.tw@gmail.com>
2017-03-14 18:51:00 +0800

    開發day1

---
+++ BootstrapJqueryValidate/Controllers/DemoController.cs
@@ -0,0 +1,45 @@
+﻿using BootstrapJqueryValidate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapJqueryValidate.Controllers
+{
+    public class DemoController : Controller
+    {
+        private DemoEntities db = new DemoEntities();
+
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddExplanation()
+        {
+            return PartialView();
+        }
+
+        public ActionResult GetList()
+        {
+            DemoViewModels demoVMs = new DemoViewModels();
+            demoVMs.GetList = new List<DemoModel>();
+            foreach(var item in db.Demo.ToList())
+            {
+                demoVMs.GetList.Add( new DemoModel
+                {
+                    Title = item.Title,
+                    Content = item.Content,
+                    CreateUser = item.CreateUser,
+                    CreateDate = item.CreateDateTime,
+                    EditUser = String.IsNullOrEmpty(item.EditUser) ? String.Empty : item.EditUser,
+                    EditDate = item.EditDateTime
+                });
+            }
+
+            return PartialView(demoVMs);
+        }
+    }
+}
---
+++ BootstrapJqueryValidate/Models/ViewModels/DemoViewModels.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BootstrapJqueryValidate.Models
+{
+    public class DemoViewModels
+    {
+        public List<DemoModel> GetList { get; set; }
+    }
+
+    public class DemoModel
+    {
+        /// <summary>
+        /// 標題
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// 內容
+        /// </summary>
+        public string Content { get; set; }
+        /// <summary>
+        /// 發表時間
+        /// </summary>
+        public DateTime? CreateDate { get; set; }
+        /// <summary>
+        /// 發表人
+        /// </summary>
+        public string CreateUser { get; set; }
+        /// <summary>
+        /// 更新時間
+        /// </summary>
+        public DateTime? EditDate { get; set; }
+        /// <summary>
+        /// 更新人
+        /// </summary>
+        public string EditUser { get; set; }
+    }
+}
